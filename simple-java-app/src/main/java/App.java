254b1443a553c042b4b506325d3537fe6cf31cb8
praveenraj2023-jes <praveenmtechse2023@gmail.com>
2026-03-31 10:01:44 +0530

    Follow standard Maven directory structure and add JUnit test

--- simple-java-app/src/main/java/App.java
+++ simple-java-app/src/main/java/App.java
@@ -1,9 +1,15 @@
 public class App {
+
+    public String getMessage() {
+        return "Hello from Java CI/CD Pipeline!";
+    }
+
     public static void main(String[] args) throws Exception {
+        App app = new App();
         System.out.println("Java CI/CD Application Started...");
         while (true) {
             Thread.sleep(5000);
-            System.out.println("Hello from Java CI/CD Pipeline!");
+            System.out.println(app.getMessage());
         }
     }
 }
---
+++ simple-java-app/src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class AppTest {
+    @Test
+    public void testGetMessage() {
+        App app = new App();
+        assertEquals("Hello from Java CI/CD Pipeline!", app.getMessage());
+    }
+}
